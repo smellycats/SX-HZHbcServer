a244ac66a55e75e86ea2e14d5d8c06bf51908e03
SmellyCat <smellycat2014@foxmail.com>
2015-09-08 13:49:30 +0800

    Add auth

--- hbc/views.py
+++ hbc/views.py
@@ -7,13 +7,22 @@ from flask_restful import reqparse, abort, Resource
 from passlib.hash import sha256_crypt
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
-from app import db, app, api, auth, limiter, logger, cache
+from app import db, app, api, auth, limiter, cache, logger, access_logger
 from models import Hbc, Hpys
 from help_func import *
 
+@app.after_request
+def after_request(response):
+    """访问信息写入日志"""
+    access_logger.info('%s - - [%s] "%s %s HTTP/1.1" %s %s'
+                       % (request.remote_addr,
+                          arrow.now().format('DD/MMM/YYYY:HH:mm:ss ZZ'),
+                          request.method, request.path, response.status_code,
+                          response.content_length))
+    return response
 
 def verify_addr(f):
-    """token验证装饰器"""
+    """IP地址白名单"""
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if not app.config['WHITE_LIST_OPEN'] or request.remote_addr in app.config['WHITE_LIST']:
@@ -40,13 +49,13 @@ def verify_token(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if not request.headers.get('Access-Token'):
-            return {'error': 'access_token error'}, 401
+            return {'status': '401.6', 'message': 'missing token header'}, 401
         token_result = verify_auth_token(request.headers['Access-Token'],
                                          app.config['SECRET_KEY'])
         if not token_result:
-            return {'error': 'access_token invalid'}, 401
+            return {'status': '401.7', 'message': 'invalid token'}, 401
         elif token_result == 'expired':
-            return {'error': 'access_token expired'}, 401
+            return {'status': '401.8', 'message': 'token expired'}, 401
         g.uid = token_result['uid']
         g.scope = set(token_result['scope'])
 
@@ -54,10 +63,9 @@ def verify_token(f):
     return decorated_function
 
 def verify_scope(f):
-    """token验证装饰器"""
+    """权限范围验证装饰器"""
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        print 'scope'
         try:
             scope = '_'.join([request.path[1:], request.method.lower()])
             print scope
@@ -72,16 +80,191 @@ def verify_scope(f):
 
 
 class Index(Resource):
-
+    
     def get(self):
         return {
-            'hbc_url': 'http://%s:%s/hbc/' % (request.remote_addr, app.config['PORT'])
+            'user_url': 'http://%s:%s/user{/user_id}' % (request.remote_addr, app.config['PORT']),
+            'scope_url': 'http://%s:%s/scope' % (request.remote_addr, app.config['PORT']),
+            'token_url': 'http://%s:%s/token' % (request.remote_addr, app.config['PORT']),
+            'hbc_url': 'http://%s:%s/hbc/：date/:hphm/:kkdd' % (request.remote_addr, app.config['PORT'])
         }, 200, {'Cache-Control': 'public, max-age=60, s-maxage=60'}
 
 
-class HbcListV1(Resource):
+class User(Resource):
+    decorators = [verify_token, limiter.limit("50/minute")]
+
+    @verify_addr
+    @verify_scope
+    def get(self, user_id):
+        user = Users.query.filter_by(id=user_id, banned=0).first()
+        if user:
+            return {'id': user.id,
+                    'username': user.username,
+                    'scope': user.scope,
+                    'date_created': str(user.date_created),
+                    'date_modified': str(user.date_modified),
+                    'banned': user.banned}, 200
+        else:
+            return {}, 404
+
+    @verify_addr
+    @verify_scope
+    def put(self, user_id):
+        verify_scope('user_put')
+        parser = reqparse.RequestParser()
+
+        parser.add_argument('scope', type=unicode, required=True,
+                            help='A scope field is require', location='json')
+        args = parser.parse_args()
+
+        # 所有权限范围
+        all_scope = set()
+        for i in Scope.query.all():
+            all_scope.add(i.name)
+        # 授予的权限范围
+        request_scope = set(request.json.get('scope', u'null').split(','))
+        # 求交集后的权限
+        u_scope = ','.join(all_scope & request_scope)
+
+        db.session.query(Users).filter_by(id=user_id).update({'scope': u_scope, 'date_modified': arrow.now().datetime})
+        db.session.commit()
+
+        user = Users.query.filter_by(id=user_id).first()
+        app.config['SCOPE_USER'][user.id] = set(user.scope.split(','))
+
+        return {
+            'id': user.id,
+            'username': user.username,
+            'scope': user.scope,
+            'date_created': str(user.date_created),
+            'date_modified': str(user.date_modified),
+            'banned': user.banned
+        }, 201
+
+
+class UserList(Resource):
+    decorators = [verify_token, limiter.limit("50/minute")]
+
+    @verify_addr
+    @verify_scope
+    def post(self):
+        if not request.json.get('username', None):
+            error = {'resource': 'Token', 'field': 'username',
+                     'code': 'missing_field'}
+            return {'message': 'Validation Failed', 'errors': error}, 422
+        if not request.json.get('password', None):
+            error = {'resource': 'Token', 'field': 'username',
+                     'code': 'missing_field'}
+            return {'message': 'Validation Failed', 'errors': error}, 422
+
+        user = Users.query.filter_by(username=request.json['username'],
+                                     banned=0).first()
+        if not user:
+            password_hash = sha256_crypt.encrypt(request.json['password'],
+                                                 rounds=app.config['ROUNDS'])
+            # 所有权限范围
+            all_scope = set()
+            for i in Scope.query.all():
+                all_scope.add(i.name)
+            # 授予的权限范围
+            request_scope = set(request.json.get('scope', u'null').split(','))
+            # 求交集后的权限
+            u_scope = ','.join(all_scope & request_scope)
+            u = Users(username=request.json['username'],
+                      password=password_hash, scope=u_scope, banned=0)
+            db.session.add(u)
+            db.session.commit()
+            return {
+                'id': u.id,
+                'username': u.username,
+                'scope': u.scope,
+                'date_created': str(u.date_created),
+                'date_modified': str(u.date_modified),
+                'banned': u.banned
+            }, 201
+        else:
+            return {'message': 'username is already esist'}, 422
+
+
+class ScopeList(Resource):
+
+    @verify_addr
+    @verify_token
+    @verify_scope
+    def get(self):
+        scope = Scope.query.all()
+        items = []
+        for i in scope:
+            items.append(row2dict(i))
+        return {'total_count': len(items), 'items': items}, 200
+
+
+def get_uid():
+    try:
+        user = Users.query.filter_by(username=request.json.get('username', ''),
+                                     banned=0).first()
+        g.uid = -1
+    except Exception as e:
+        logger.error(e)
+        raise
+    if user:
+        if sha256_crypt.verify(request.json.get('password', ''), user.password):
+            g.uid = user.id
+            g.scope = user.scope
+    return str(g.uid)
+
+
+class TokenList(Resource):
+    decorators = [limiter.limit("5/hour", get_uid), verify_addr]
+
+    def post(self):
+        if not request.json.get('username', None):
+            error = {'resource': 'Token', 'field': 'username',
+                     'code': 'missing_field'}
+            return {'message': 'Validation Failed', 'errors': error}, 422
+        if not request.json.get('password', None):
+            error = {'resource': 'Token', 'field': 'username',
+                     'code': 'missing_field'}
+            return {'message': 'Validation Failed', 'errors': error}, 422
+        if g.uid == -1:
+            return {'message': 'username or password error'}, 422
+        s = Serializer(app.config['SECRET_KEY'],
+                       expires_in=app.config['EXPIRES'])
+        token = s.dumps({'uid': g.uid, 'scope': g.scope.split(',')})
+        return {
+            'uid': g.uid,
+            'access_token': token,
+            'token_type': 'self',
+            'scope': g.scope,
+            'expires_in': app.config['EXPIRES']
+        }, 201, {'Cache-Control': 'no-store', 'Pragma': 'no-cache'}
+
+
+class HbcApi(Resource):
     decorators = [limiter.limit("600/minute"), verify_addr]
 
+    @verify_token
+    def get(self, date, hphm, kkdd):
+        try:
+            hbc = Hbc.query.filter(Hbc.date==date, Hbc.hphm==hphm,
+                                   Hbc.kkdd_id.startswith(kkdd),
+                                   Hbc.imgpath != '').all()
+        except Exception as e:
+            logger.error(e)
+
+        items = []
+        for i in hbc:
+            items.append({'id': i.id, 'jgsj': str(i.jgsj), 'hphm': i.hphm,
+                          'kkdd_id': i.kkdd_id, 'imgpath': i.imgpath})
+
+        return {'total_count': len(items), 'items': items}, 200
+        
+        
+        
+class HbcList(Resource):
+    decorators = [limiter.limit("600/minute"), verify_addr]
+
+    @verify_token
     def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument('jgsj', type=unicode, required=True,
@@ -123,7 +306,7 @@ class HbcListV1(Resource):
 
 
 api.add_resource(Index, '/')
-api.add_resource(HbcListV1, '/hbc')
-
+api.add_resource(HbcApi, '/hbc/<string:date>/<string:hphm>/<string:kkdd>')
+api.add_resource(HbcList, '/hbc')
 
 
